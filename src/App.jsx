008e8fa4e6ecc3613059c9d66a7902b85a967951
chiedozieu <chiedozieugwor@gmail.com>
2024-05-02 14:19:26 +0100

    creating navbar, routes and Links

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Services from "./pages/Services"
+import Contact from "./pages/Contact"
+import Navbar from "./component/Navbar"
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>App</div>
+   
+  <BrowserRouter>
+  <Navbar />
+  <Routes>
+    <Route path="/" element=<Home /> > </Route>
+    <Route path="/about" element=<About /> > </Route>
+    <Route path="/contact" element=<Contact /> > </Route>
+    <Route path="/services" element=<Services /> > </Route>
+    
+
+   
+  </Routes>
+
+  </BrowserRouter>
+
   )
 }
 
