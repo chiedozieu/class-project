031f866efbc52caa6d646e529f0ae46156966704
chiedozieu <chiedozieugwor@gmail.com>
2024-05-08 09:46:42 +0100

    add fonts, finish footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import bg from '../images/bgfooter.png'
 import '../css/footer.css'
+import { PiFacebookLogo } from "react-icons/pi";
+import { RiTwitterXFill } from "react-icons/ri";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { GrLinkedin } from "react-icons/gr";
+import { FaWhatsappSquare } from "react-icons/fa";
+import { BiSolidPhoneCall } from "react-icons/bi";
+import { TfiEmail } from "react-icons/tfi";
+
 
 const Footer = () => {
   return (
     <div>
-       <div className='mt-4' style={{backgroundImage: `url(${bg})`
-}} >
+       <div className='mt-4' style={{backgroundImage: `url(${bg})`}}>
                 <div className='d-flex justify-content-around '>
                     <div className="footer-info">
                         <h2>KNOW US</h2>
@@ -31,14 +38,13 @@ const Footer = () => {
                             <h2>GET IN TOUCH</h2>
                             <p>N0 4 Bimpe Lagbaja Street, Onoja lagos State.</p>
                         <ul>
-                            <li>+234 773 773 2222</li>
-                            <li>+234 773 773 2223</li>
-                            <li>@ojsounds</li>
-                            <li>@ojsounds</li>
-                            <li>@ojsounds</li>
-                            <li>@ojsounds</li>
-                            <li></li>
-                            <li></li>
+                            <li><a href="#"><span className='footer-icons phone'><BiSolidPhoneCall /></span> +234 773 773 2222</a></li>
+                            <li><a href="#"><span className='footer-icons whatsapp'><FaWhatsappSquare /></span> +234 773 773 2223</a></li>
+                            <li><a href="#"><span className='footer-icons facebook'><PiFacebookLogo /></span> @ojsounds</a></li>
+                            <li><a href="#"><span className='footer-icons x'><RiTwitterXFill /></span> @ojsounds</a></li>
+                            <li><a href="#"><span className='footer-icons instagram'><FaSquareInstagram /></span> @ojsounds</a></li>
+                            <li><a href="#"><span className='footer-icons linkedin'><GrLinkedin /></span> @ojsounds</a></li>
+                          
                         </ul>
                     </div>
                 </div>
@@ -46,9 +52,9 @@ const Footer = () => {
           
            
          
-            <div id="footer-bottom">
+            <div id="footer-bottom" className='d-flex justify-around'>
                 <p>Copyright © 2023 New Horizon All rights reserved.</p>
-                <a href="mailto: support@ojsounds.com">support@ojsounds.com</a>
+                <a href="mailto: support@ojsounds.com"><span className='footer-icons email'><TfiEmail />&nbsp;&nbsp;&nbsp;</span>support@ojsounds.com</a>
             </div>
         </div>
     </div>
